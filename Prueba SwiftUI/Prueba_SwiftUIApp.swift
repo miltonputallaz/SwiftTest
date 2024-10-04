f64930a26d9f5361d48da25e4b9fdbec0b680629
Milton <miltonputallaz@gmail.com>
2024-10-04 12:30:23 -0300

    Initial Commit

---
+++ Prueba SwiftUI/Prueba_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prueba_SwiftUIApp.swift
+//  Prueba SwiftUI
+//
+//  Created by Milton Putallaz on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Prueba_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
